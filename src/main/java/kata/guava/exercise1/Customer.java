68923581d9cebe585083e5cd90499e83f13afbce
Tommy1199 <sascha.selzer@gmail.com>
2016-04-24 18:23:26 +0200

    Add instruction for getPreferredName and reformat files.

--- src/main/java/kata/guava/exercise1/Customer.java
+++ src/main/java/kata/guava/exercise1/Customer.java
@@ -2,118 +2,121 @@ package kata.guava.exercise1;
 
 public class Customer {
 
-	private final static String INVALID_NAME = "";
+    private final static String INVALID_NAME = "";
 
-	private final String firstName;
-	private final String lastName;
-	private final String nickName;
+    private final String firstName;
+    private final String lastName;
+    private final String nickName;
 
     /**
      * This is a typical constructor with basic checks of the parameters. Try to refactor it with the help of
      * {@link com.google.common.base.Preconditions}.
-     *
+     * <p>
      * Hint: With JDK7 you can replace the null checks with Objects#requireNonNull instead of
      * {@link com.google.common.base.Preconditions#checkNotNull(Object)}, but it has no equivalent for checking the
-     * argument. So I would still prefer to use all checks from guava to avoid mixing several classes up.
+     * argument. So I would still prefer to use all checks from guava to avoid mixing several classes up for the same purpose.
      */
-	public Customer(String firstName, String lastName, String nickName) {
-		if (firstName == null) {
-			throw new NullPointerException("firstName cannot be null");
-		}
-		if (INVALID_NAME.equals(firstName)) {
-			throw new IllegalArgumentException("firstName cannot be empty");
-		}
-		if (lastName == null) {
-			throw new NullPointerException("lastName cannot be null");
-		}
-		if (INVALID_NAME.equals(lastName)) {
-			throw new IllegalArgumentException("lastName cannot be empty");
-		}
-		if (INVALID_NAME.equals(nickName)) {
-			throw new IllegalArgumentException("nickName cannot be empty");
-		}
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.nickName = nickName;
-	}
+    public Customer(String firstName, String lastName, String nickName) {
+        if (firstName == null) {
+            throw new NullPointerException("firstName cannot be null");
+        }
+        if (INVALID_NAME.equals(firstName)) {
+            throw new IllegalArgumentException("firstName cannot be empty");
+        }
+        if (lastName == null) {
+            throw new NullPointerException("lastName cannot be null");
+        }
+        if (INVALID_NAME.equals(lastName)) {
+            throw new IllegalArgumentException("lastName cannot be empty");
+        }
+        if (INVALID_NAME.equals(nickName)) {
+            throw new IllegalArgumentException("nickName cannot be empty");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getNickName() {
-		return nickName;
-	}
+    public String getNickName() {
+        return nickName;
+    }
 
-	public String getPreferredName() {
-		if (nickName != null) {
-			return nickName;
-		}
-		return firstName;
-	}
+    /**
+     * Shorten this code with {@link com.google.common.base.MoreObjects#firstNonNull(Object, Object)}.
+     */
+    public String getPreferredName() {
+        if (nickName != null) {
+            return nickName;
+        }
+        return firstName;
+    }
 
     /**
      * This is the way how a auto-generated hashCode method from an IDE looks like. Replace the code
      * with the help of {@link com.google.common.base.Objects#hashCode(Object...)}. This improves the readability
      * as you get rid of magic numbers and calculation.
-     *
+     * <p>
      * Hint: If you are using already JDK7, use the contained Objects#hash of the JDK instead.
      */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
 
     /**
      * This is also an auto-generated method which is pretty hard to read as it is always a mix of null checks and
      * equality checks. Use {@link com.google.common.base.Objects#equal(Object, Object)} to improve it.
-     *
+     * <p>
      * Hint: Again, if you are using JDK 7, use the Objects#equals method in the JDK instead.
      */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Customer other = (Customer) obj;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Customer other = (Customer) obj;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        return true;
+    }
 
     /**
      * Use {@link com.google.common.base.MoreObjects.ToStringHelper} to create the same output than below.
      */
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder().append("Customer{").append("lastName=").append(lastName)
-				.append(", firstName=").append(firstName);
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder().append("Customer{").append("lastName=").append(lastName)
+                .append(", firstName=").append(firstName);
 
-		if (nickName != null) {
-			builder.append(", nickName=").append(nickName);
-		}
+        if (nickName != null) {
+            builder.append(", nickName=").append(nickName);
+        }
 
-		builder.append("}");
+        builder.append("}");
 
-		return builder.toString();
-	}
+        return builder.toString();
+    }
 }
--- src/test/java/kata/guava/exercise1/CustomerTest.java
+++ src/test/java/kata/guava/exercise1/CustomerTest.java
@@ -1,6 +1,7 @@
 package kata.guava.exercise1;
 
 import org.junit.Test;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,67 +9,67 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CustomerTest {
 
-	@Test(expected = NullPointerException.class)
-	public void shouldThrowNullPointerWhenFirstNameIsNull() {
-		new Customer(null, "Maier", "Pit");
-	}
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerWhenFirstNameIsNull() {
+        new Customer(null, "Maier", "Pit");
+    }
 
-	@Test(expected = NullPointerException.class)
-	public void shouldThrowNullPointerWhenLastNameIsNull() {
-		new Customer("Peter", null, "Pit");
-	}
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerWhenLastNameIsNull() {
+        new Customer("Peter", null, "Pit");
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void shouldThrowIllegalArgumentExceptionWhenFirstNameIsEmpty() {
-		new Customer("", "Maier", "Pit");
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionWhenFirstNameIsEmpty() {
+        new Customer("", "Maier", "Pit");
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void shouldThrowIllegalArgumentExceptionWhenLastNameIsEmpty() {
-		new Customer("Peter", "", "Pit");
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionWhenLastNameIsEmpty() {
+        new Customer("Peter", "", "Pit");
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void shouldThrowIllegalArgumentExceptionWhenNickNameIsEmpty() {
-		new Customer("Peter", "Maier", "");
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionWhenNickNameIsEmpty() {
+        new Customer("Peter", "Maier", "");
+    }
 
-	@Test
-	public void shouldAddAllCustomerInfoToString() {
-		Customer customer = new Customer("Peter", "Maier", "Pit");
-		assertThat(customer.toString()).isEqualTo("Customer{lastName=Maier, firstName=Peter, nickName=Pit}");
-	}
+    @Test
+    public void shouldAddAllCustomerInfoToString() {
+        Customer customer = new Customer("Peter", "Maier", "Pit");
+        assertThat(customer.toString()).isEqualTo("Customer{lastName=Maier, firstName=Peter, nickName=Pit}");
+    }
 
-	@Test
-	public void shouldIgnoreNickNameIfNullInToString() {
-		Customer customer = new Customer("Peter", "Maier", null);
-		assertThat(customer.toString()).isEqualTo("Customer{lastName=Maier, firstName=Peter}");
-	}
+    @Test
+    public void shouldIgnoreNickNameIfNullInToString() {
+        Customer customer = new Customer("Peter", "Maier", null);
+        assertThat(customer.toString()).isEqualTo("Customer{lastName=Maier, firstName=Peter}");
+    }
 
-	@Test
-	public void shouldReturnNickNameAsPreferredNameWhenNotNull() {
-		Customer customer = new Customer("Peter", "Maier", "Pit");
-		assertThat(customer.getPreferredName()).isEqualTo("Pit");
-	}
+    @Test
+    public void shouldReturnNickNameAsPreferredNameWhenNotNull() {
+        Customer customer = new Customer("Peter", "Maier", "Pit");
+        assertThat(customer.getPreferredName()).isEqualTo("Pit");
+    }
 
-	@Test
-	public void shouldReturnFirstNameAsPreferredNameWhenNickNameIsNull() {
-		Customer customer = new Customer("Peter", "Maier", null);
-		assertThat(customer.getPreferredName()).isEqualTo("Peter");
-	}
+    @Test
+    public void shouldReturnFirstNameAsPreferredNameWhenNickNameIsNull() {
+        Customer customer = new Customer("Peter", "Maier", null);
+        assertThat(customer.getPreferredName()).isEqualTo("Peter");
+    }
 
-	@Test
-	public void shouldOnlyTakeFirstNameAndLastNameForHashing() {
-		Set<Customer> customers = new HashSet<Customer>();
-		customers.add(new Customer("Peter", "Maier", "Pit"));
-		customers.add(new Customer("Peter", "Maier", null));
-		assertThat(customers.size()).isEqualTo(1);
-	}
+    @Test
+    public void shouldOnlyTakeFirstNameAndLastNameForHashing() {
+        Set<Customer> customers = new HashSet<Customer>();
+        customers.add(new Customer("Peter", "Maier", "Pit"));
+        customers.add(new Customer("Peter", "Maier", null));
+        assertThat(customers.size()).isEqualTo(1);
+    }
 
-	@Test
-	public void shouldIgnoreNickNameForEquality() {
-		Customer customerWithNickName = new Customer("Peter", "Maier", "Pit");
-		Customer customerWithoutNickname = new Customer("Peter", "Maier", null);
-		assertThat(customerWithNickName).isEqualTo(customerWithoutNickname);
-	}
+    @Test
+    public void shouldIgnoreNickNameForEquality() {
+        Customer customerWithNickName = new Customer("Peter", "Maier", "Pit");
+        Customer customerWithoutNickname = new Customer("Peter", "Maier", null);
+        assertThat(customerWithNickName).isEqualTo(customerWithoutNickname);
+    }
 }
